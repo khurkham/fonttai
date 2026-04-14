3cafb3e57737b2d9a1e157859d5608edd865f1bc
khurkham <30972820+khurkham@users.noreply.github.com>
2026-04-15 03:22:31 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 export default defineConfig({
   plugins: [react(), cloudflare()],
   optimizeDeps: {
-    exclude: ["hono"],
+    exclude: ['hono', 'hono/cors', 'hono/cookie'],
   },
 });
